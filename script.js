7ccef0e821f5ae2dd3c7650feffe51e040973b13
Atith794 <68183452+Atith794@users.noreply.github.com>
2024-03-13 21:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+
+function handleClick(event){
+	event.preventDefault();
+	let name = document.getElementById("name").value;
+	let year = document.getElementById("year").value;
+	let id = document.getElementById("url");
+	
+	
+	if(name !== '' && year !== ''){
+		id.innerHTML = "";
+		id.innerHTML = `https://localhost:8080/?name=${name}&year=${year}`;
+	}
+	else if (name !== '' && year === '') {
+		id.innerHTML = "";
+		id.innerHTML = `https://localhost:8080/?name=${name}`;
+	}
+	else if (name === '' && year !== '') {
+		id.innerHTML = "";
+		id.innerHTML = `https://localhost:8080/?year=${year}`;
+	}
+}
+
